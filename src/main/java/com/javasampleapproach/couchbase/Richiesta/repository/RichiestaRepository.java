80db648e70b638ccc0256078336671af767b6cee
Unknown <vincenzodamico93@tiscali.it>
2018-12-02 13:13:46 +0100

    optimize import

--- src/main/java/com/javasampleapproach/couchbase/Richiesta/repository/RichiestaRepository.java
+++ src/main/java/com/javasampleapproach/couchbase/Richiesta/repository/RichiestaRepository.java
@@ -2,11 +2,8 @@ package com.javasampleapproach.couchbase.Richiesta.repository;
 
 import com.javasampleapproach.couchbase.Richiesta.model.Richiesta;
 import org.springframework.data.couchbase.core.query.N1qlPrimaryIndexed;
-import org.springframework.data.couchbase.core.query.Query;
 import org.springframework.data.couchbase.core.query.ViewIndexed;
 import org.springframework.data.couchbase.repository.CouchbasePagingAndSortingRepository;
-
-import java.util.List;
 
 @N1qlPrimaryIndexed
 @ViewIndexed(designDoc = "richiesta")
